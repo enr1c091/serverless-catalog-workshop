1a5b4559b983bad4fcec36a131f3f827d50af4b7
Enrico Bergamo <enricosb@amazon.com>
2020-02-25 19:31:20 -0300

    Fix Locust files

--- sam/nodejs/locust-script.py
+++ sam/nodejs/locust-script.py
@@ -6,6 +6,7 @@ class ConverterTasks(TaskSet):
         self.client.get('items/')
 # Receives the URL from CloudFormation output variable containing "/" as last character so omit it as first char 
 
+# Add tests to more resources as you require
 #    @task
 #    def day_to_minute(self):
 #        self.client.get('items/1')
@@ -13,5 +14,3 @@ class ConverterTasks(TaskSet):
 class ApiUser(HttpLocust):
     task_set = ConverterTasks
     wait_time = between(1,3)
-
-#locust -f locust-script.py -H https://iz2j9tfy6c.execute-api.us-east-1.amazonaws.com/Prod --no-web -c 1000 -r 100 --run-time 1m
